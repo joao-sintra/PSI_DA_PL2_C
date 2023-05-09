a5aba95fdbb644d078c8ff527ee90291198de503
Ruben Amaral <2220848@my.ipleiria.pt>
2023-05-09 10:38:08 +0100

    Classes feitas

--- CineGest/Form1.cs
+++ CineGest/Form1.cs
@@ -14,22 +14,22 @@ namespace CineGest {
     public partial class Atendimento : Form {
         public Atendimento() {
             InitializeComponent();
-            ListViewItem item1 = new ListViewItem();
-            int colunas = 12;
-            int linhas = 12;
+            //ListViewItem item1 = new ListViewItem();
+            //int colunas = 12;
+            //int linhas = 12;
             
-            for (int i = 0; i <= linhas; i++) {
-                listViewLugares.Columns.Add(i.ToString(),40, HorizontalAlignment.Center);
-                for (int j = 1; j <= colunas; j++) {
-                item1 = new ListViewItem(" " + i.ToString() + " ");
-                listViewLugares.Items.AddRange(new ListViewItem[] { item1 });
-                    listViewLugares.Items[i].SubItems.Add("99");
+            //for (int i = 0; i <= linhas; i++) {
+            //    listViewLugares.Columns.Add(i.ToString(),40, HorizontalAlignment.Center);
+            //    for (int j = 1; j <= colunas; j++) {
+            //    item1 = new ListViewItem(" " + i.ToString() + " ");
+            //    listViewLugares.Items.AddRange(new ListViewItem[] { item1 });
+            //        listViewLugares.Items[i].SubItems.Add("99");
 
-            }
+            //}
 
-            item1.SubItems[0].BackColor = Color.Green;
+            //item1.SubItems[0].BackColor = Color.Green;
 
-            }
+            //}
         }
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e) {
